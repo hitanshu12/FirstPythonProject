346912fe468db530a1312e0cae1b0df85175fbdf
hitanshu12 <write2hitanshu@gmail.com>
2025-12-04 20:25:44 +0530

    Commit V 1.2

--- caeserCipher.py
+++ caeserCipher.py
@@ -5,7 +5,8 @@
 
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # direction = input("Type 'encode' to to encrypt and 'decode' to dycript: \n ").lower()
 # text = input("Type your message: \n").lower()
